10d321528070d2d2904131861de56cb00c2c2a18
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-09 10:15:02 +0530

    Loops 1

--- 04-statements/Demo16.java
+++ 04-statements/Demo16.java
@@ -16,6 +16,25 @@ public class Demo16 {
             + + + + +
          */
 
+        int y = 0;
+        int k = 5;
+        boolean reverse = false;
+        while (y++ < 9){
+
+          int x = 0;
+          while (x++ < k){
+            System.out.print("+ ");
+          }
+
+          if (!reverse){
+            k--;
+            if (k == 1) reverse = true;
+          }else{
+            k++;
+          }
+          System.out.println();
+        }
+
          /*
             Single Loop, Nested Loops
             Via + (Space)
@@ -39,7 +58,9 @@ public class Demo16 {
             + +       + +
             + + +   + + +
             + + + + + + +
-          */          
+          */  
+          
+
 
          /* 
             Nested Loops
