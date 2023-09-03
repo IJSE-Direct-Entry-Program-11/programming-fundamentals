5354034d4338e44c262d156e7036d0c14f560699
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-03 19:58:11 +0530

    Finalize the Programming Fundamentals

---
+++ 02-data-types,literals,variables,constants/Demo23.java
@@ -0,0 +1,15 @@
+public class Demo23 {
+    public static void main(String[] args) {
+        
+        Integer i = 10;
+        
+        Byte b = 10; // byte temp = 10 -> new Byte(temp);
+        
+        //Integer a = b;
+        
+        int x = b;
+
+        //Short s = i;
+
+    }
+}
